6e7cb94c9476858667da0960dc1f8f43610bbe4d
Eliezer Nascimento <eliezergarbin1@gmail.com>
2023-01-23 10:10:20 -0300

    configurações iniciais

---
+++ EclesiaAPI/Model/Contributions.cs
@@ -0,0 +1,10 @@
+﻿namespace EclesiaAPI.Model
+{
+    public class Contributions
+    {
+        public int Id { get; set; }
+        public DateTime ContributionDate { get; set; } 
+        public string ContributionForm { get; set; }
+        public int Value { get; set; }
+    }
+}
---
+++ EclesiaAPI/Model/Members.cs
@@ -0,0 +1,22 @@
+﻿namespace EclesiaAPI.Model
+{
+    public class Members
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int RG { get; set; }
+        public int CPF { get; set; }
+        public string Affiliation { get; set; }
+        public string MaritalStatus { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string baptism { get; set; }
+        public string Receipt { get; set; }
+        public int CEP { get; set; }
+        public string UF { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Neighborhood { get; set; }
+
+
+    }
+}
