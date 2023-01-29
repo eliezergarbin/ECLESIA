1b14c4b5f6fcaa8e213c0600aca0378ead518c71
Eliezer Nascimento <eliezergarbin1@gmail.com>
2023-01-28 23:38:26 -0300

    criando models

---
+++ Server/Models/Contribution.cs
@@ -0,0 +1,12 @@
+﻿namespace Server.Models
+{
+    public class Contribution
+    {
+        public int Id { get; set; }
+        public DateTime ContributionDate { get; set; }
+        public string ContributionForm { get; set; }
+        public int Value { get; set; }
+
+        public Member Member { get; set; }
+    }
+}
---
+++ Server/Models/Member.cs
@@ -0,0 +1,24 @@
+﻿namespace Server.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int RG { get; set; }
+        public int CPF { get; set; }
+        public string Affiliation { get; set; }
+        public string MaritalStatus { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string baptism { get; set; }
+        public string Receipt { get; set; }
+        public int CEP { get; set; }
+        public string UF { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Neighborhood { get; set; }
+
+        public List<Contribution> Contributions { get; set; }
+        public Position Position { get; set; }
+
+    }
+}
