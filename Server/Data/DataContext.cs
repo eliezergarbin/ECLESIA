219a138a7d1f63fcd45a2e1530e452cdb940934f
Eliezer Nascimento <eliezergarbin1@gmail.com>
2023-02-01 22:55:30 -0300

    criando context

---
+++ Server/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Contribution> Contributions { get; set; }
+        public DbSet<Position> Positions { get; set; }
+    }
+}
